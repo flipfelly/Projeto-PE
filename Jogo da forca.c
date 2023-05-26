a6f257f4ba9ae736bb8ea3229212e84fbc07ae31
Mariana Martins <marianamartiyns@gmail.com>
2023-05-26 15:36:29 -0300

    update

--- Jogo da forca.c
+++ Jogo da forca.c
@@ -1,34 +1,106 @@
 #include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+// Função da intercafe do boneco(0 = não tem nada na forca, 1 = tem a cabeça...)
+void forca(int estado){
+    if (estado == 0){
+        printf("\n-=======");
+        printf("\n||     |");
+        printf("\n||");
+        printf("\n||");
+        printf("\n||");
+        printf("\n||");
+        printf("\n||");
+        printf("\n===");
+    } else if(estado==1){
+        printf("\n-=======");
+        printf("\n||     |");
+        printf("\n||     0");
+        printf("\n||");
+        printf("\n||");
+        printf("\n||");
+        printf("\n||");
+        printf("\n===");
+    } else if(estado==2){
+        printf("\n-=======");
+        printf("\n||     |");
+        printf("\n||     0");
+        printf("\n||     |");
+        printf("\n||");
+        printf("\n||");
+        printf("\n||");
+        printf("\n===");
+    }else if(estado==3){
+        printf("\n-=======");
+        printf("\n||     |");
+        printf("\n||     0");
+        printf("\n||    /|");
+        printf("\n||");
+        printf("\n||");
+        printf("\n||");
+        printf("\n===");
+    }else if(estado==4){
+        printf("\n-=======");
+        printf("\n||     |");
+        printf("\n||     0");
+        printf("\n||    /|\\");
+        printf("\n||");
+        printf("\n||");
+        printf("\n||");
+        printf("\n===");
+    }else if(estado==5){
+        printf("\n-=======");
+        printf("\n||     |");
+        printf("\n||     0");
+        printf("\n||    /|\\");
+        printf("\n||    /");
+        printf("\n||");
+        printf("\n||");
+        printf("\n===");
+    }else if(estado==6){
+        printf("\n-=======");
+        printf("\n||     |");
+        printf("\n||     0");
+        printf("\n||    /|\\");
+        printf("\n||    / \\");
+        printf("\n||");
+        printf("\n||  PERDEU!");
+        printf("\n===");
+    }
+}
 
 int main(void) {
 
-  char palavra[100];
-  int opcao, strc;
-  FILE *arq;
+    setlocale(LC_ALL, "Portuguese");
+    char palavra[100];
+    int opcao, strc;
+    FILE *arq;
 
-  printf("--- Seja bem-vindo ao jogo da forca ---");
-  printf("\n\n---MENU---\n1- Configurar\n2- Jogar\n----------");
-  printf("\n\nDigite uma das opções do menu:");
-  scanf("%d", &opcao);
+    printf("--- Bem-vindo ao Jogo da Forca ---");
+    printf("\n\n ------ MENU ------ \n[ 1 ] - Configurar\n[ 2 ] - Jogar\n-------------------");
+    printf("\n\nDigite uma das opções do menu: ");
+    scanf("%d", &opcao);
 
-  switch (opcao) {
+    switch (opcao) {
+
     case 1:
-      printf("\n\n---------------------------");
-      printf("\n Seja bem-vindo as configurações ");
+      printf("\n\n--------------------------------");
+      printf("\n Seja Bem-Vindo as Configurações ");
 
       arq = fopen("Banco_palavras.txt", "w");
       do {
-      printf("\n---------------------------");
+      printf("\n--------------------------------\n\n[ Digite 'sair' para encerrar ]");
 
-      printf("\n\nQual palavra deseja adicionar (Digite 'sair' para encerrar)?");
+      printf("\nQual palavra deseja adicionar? ");
       scanf("%s", palavra);
 
       strc = strcmp(palavra, "sair");
 
       if (strc == 0) {
         printf(
-            "\n---------------------------\nObrigado por adicionar novas "
-            "palavras!\n---------------------------\nConfiguração encerrada");
+            "\n--------------------------------------\nObrigado por adicionar novas "
+            "palavras!\n--------------------------------------\n\nConfiguração encerrada.");
       } else {
         fprintf(arq, "%s\n", palavra);
       }
@@ -41,15 +113,75 @@ int main(void) {
 
     case 2 :
 
-      printf("--- Bem vindo ao Jogo ---");
+      printf("\n--- Bem vindo ao Jogo ---\n ");
+
+      char p[100], letra;
+
+      // Pegando a palavra
+      printf("\nInforme a palavra: ");
+      scanf("%s", p); // não pega espaço
+      printf("\nA palavra foi escolhida!\n");
+      printf("A palavra tem %lu caracteres.\n", strlen(p));
+
+      char tela[100];
+      strcpy(tela, p);
+
+      for(int i=0; i < strlen(tela); i++){
+        tela[i] = '_';
+      }
+
+      int erros = 0;
+      while(1){
+
+        //imprime a forca
+        forca(erros);
+
+        // imprime os underlines para cada letra
+        printf("\nAdvinhe:  ");
+        for(int i=0; i < strlen(tela); i++){
+            printf("%c ", tela[i]);
+        }
+
+        // recebe a letra
+        printf("\n\nLetra: ");
+        scanf(" %c", &letra);
+
+        //verifica se a letra esta na palavra
+
+        int possivel_erro = 1; // 1 = sim, 0 = não
+        for(int i=0; i < strlen(tela); i++){
+
+          if(letra == p[i]){ //a letra ta certa.
+              tela[i] = letra;
+              possivel_erro=0;
+          }
+        }
+
+        if(possivel_erro == 1){
+          erros++;
+        }
+
+        // verifica se o jogo acabou por erro
+        if(erros == 6){ //perdeu
+          forca(erros);
+          break;
+        }
+
+        // verifica se tela == palavra, logo, jogo ganho.
+        if(strcmp(tela, p)==0){
+          printf("\nPalavra:  ");
+          for(int i=0; i < strlen(tela); i++){
+            printf("%c ", tela[i]);
+          }
+          printf("\nVocê Ganhou!\n\n");
+          break;
+        }
+      }
+
       break;
 
     default:
+      printf("Valor não encontrado!");
 
-    printf("Valor não encontrado!");
-
-
-  }
-
-
+    }
 }
