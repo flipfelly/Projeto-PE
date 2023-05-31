71e7e7a9f248e9bd2326553bcf84683d97ab3f34
Felipe Gontijo Siqueira <felianginho@gmail.com>
2023-05-31 19:56:47 -0300

    Atualização do jogo

--- Jogo da forca.c
+++ Jogo da forca.c
@@ -10,7 +10,7 @@ char *escolhePalavra(char nomeArquivo[], char escolha[]) {
   FILE *arq;
 
   // Abre o arquivo
-  if ((arq = fopen("Banco_palavras.txt", "r")) == NULL)
+  if ((arq = fopen(nomeArquivo, "r")) == NULL)
     puts("Erro na abertura do arquivo!\n\n");
   else {
     do {
@@ -109,68 +109,37 @@ void forca(int estado) {
 int main(void) {
 
   setlocale(LC_ALL, "Portuguese");
-  char palavra[100];
-  int opcao, strc;
+  char palavra[100], resp, repeat;
+  int opcao, strc, opcao2;
   FILE *arq;
-  printf("--- Bem-vindo ao Jogo da Forca ---");
-  printf("\n\n ------ MENU ------ \n[ 1 ] - Configurar\n[ 2 ] - "
-         "Jogar\n-------------------");
+
+  do{
+  printf("\n--- Bem-vindo ao Jogo da Forca ---");
+  printf("\n\n ------ MENU ------ \n\n [ 1 ] - Adjetivos \n [ 2 ] - Objetos \n [ 3 ] - Animais \n\n ------------------\n\n [ 4 ] - Configurar"); 
   printf("\n\nDigite uma das opções do menu: ");
   scanf("%d", &opcao);
 
   switch (opcao) {
 
   case 1:
-    printf("\n\n--------------------------------");
-    printf("\n Seja Bem-Vindo as Configurações ");
 
-    arq = fopen("Banco_palavras.txt", "a");
-    do {
-      printf("\n--------------------------------\n\n[ Digite 'sair' para "
-             "encerrar ]");
-
-      printf("\nQual palavra deseja adicionar? ");
-      scanf("%s", palavra); //recebe a palavra a ser gravada
-
-      strc = strcmp(palavra, "sair"); //Compara a string e retorna um valor para o encerramento ou não.
-
-      if (strc == 0) {
-        //encerra a configuração
-        printf("\n--------------------------------------\nObrigado por "
-               "adicionar novas "
-               "palavras!\n--------------------------------------"
-               "\n\nConfiguração encerrada.");
-      } else {
-
-        fputs(palavra, arq); //escreve a palavra no arquivo
-        fputs("\n", arq); // adiciona quebra de linha
-      }
-
-    } while (strc != 0);
-
-    fclose(arq); // fecha o arquivo de escrita
-
-    break;
-
-  case 2:
-
-    printf("\n--- Bem vindo ao Jogo ---\n ");
+    printf("\n--- INÍCIO DE JOGO ---\n\n ");
 
     char p[100], letra, escolha[100];
 
     FILE *arq_ler;
 
-    arq_ler = fopen("Banco_palavras.txt", "r");//abre o arquivo com as palavras
+    arq_ler = fopen("banco_adjetivos.txt", "r");//abre o arquivo com as palavras
 
     if ((arq_ler) == NULL) {
       printf("Erro para abrir arquivo");
       exit(1);
     } else {
-      escolhePalavra("Banco_palavras.txt", escolha);// chama a funcção para escolha aleatoria de palavras
+      escolhePalavra("banco_adjetivos.txt", escolha);// chama a funcção para escolha aleatoria de palavras
     }
-    strcpy(p, escolhePalavra("Banco_palavras.txt", escolha));//Copia o valor para a palavra a ser utilizada
+    strcpy(p, escolhePalavra("banco_adjetivos.txt", escolha));//Copia o valor para a palavra a ser utilizada
 
-    printf("A palavra tem %lu caracteres.\n", strlen(p));
+    printf("\nA palavra tem %lu caracteres.\n", strlen(p));
 
     char tela[100];
     strcpy(tela, p);
@@ -222,14 +191,291 @@ int main(void) {
         for (int i = 0; i < strlen(tela); i++) {
           printf("%c ", tela[i]);
         }
-        printf("\nVocê Ganhou!\n\n");
+      printf("\n\n    *  *  *\n!!! VITÓRIA !!!\n    *  *  *\n");
         break;
       }
     }
+  fclose(arq_ler);
+  break;
+    
+  case 2:
+
+    printf("\n--- INÍCIO DE JOGO ---\n\n ");
+
+    char p2[100], letra2, escolha2[100];
+
+    FILE *arq_ler2;
+
+    arq_ler2 = fopen("banco_objetos.txt", "r");//abre o arquivo com as palavras
+
+    if ((arq_ler2) == NULL) {
+      printf("Erro para abrir arquivo");
+      exit(1);
+    } else {
+      escolhePalavra("banco_objetos.txt", escolha2);// chama a funcção para escolha aleatoria de palavras
+    }
+    strcpy(p2, escolhePalavra("banco_objetos.txt", escolha2));//Copia o valor para a palavra a ser utilizada
+
+    printf("A palavra tem %lu caracteres.\n", strlen(p2));
+
+    char tela2[100];
+    strcpy(tela2, p2);
+
+    for (int i = 0; i < strlen(tela2); i++) {
+      tela2[i] = '_';
+    }
+
+    int erros2 = 0;
+    while (1) {
+
+      // imprime a forca
+      forca(erros2);
+
+      // imprime os underlines para cada letra
+      printf("\nAdvinhe:  ");
+      for (int i = 0; i < strlen(tela2); i++) {
+        printf("%c ", tela2[i]);
+      }
+
+      // recebe a letra
+      printf("\n\nLetra: ");
+      scanf(" %c", &letra2);
+
+      // verifica se a letra esta na palavra
+
+      int possivel_erro2 = 1; // 1 = sim, 0 = não
+      for (int i = 0; i < strlen(tela2); i++) {
+
+        if (letra2 == p2[i]) { // a letra ta certa.
+          tela2[i] = letra2;
+          possivel_erro2 = 0;
+        }
+      }
+
+      if (possivel_erro2 == 1) {
+        erros2++;
+      }
+
+      // verifica se o jogo acabou por erro
+      if (erros2 == 6) { // perdeu
+        forca(erros2);
+        break;
+      }
+
+      // verifica se tela == palavra, logo, jogo ganho.
+      if (strcmp(tela2, p2) == 0) {
+        printf("\nPalavra:  ");
+        for (int i = 0; i < strlen(tela2); i++) {
+          printf("%c ", tela2[i]);
+        }
+        printf("\n\n    *  *  *\n!!! VITÓRIA !!!\n    *  *  *\n");
+        break;
+      }
+    }
+    fclose(arq_ler2);
+    break;
+    
+    case 3:
+
+    printf("\n--- INÍCIO DE JOGO ---\n\n ");
+
+    char p3[100], letra3, escolha3[100];
+
+    FILE *arq_ler3;
+
+    arq_ler3 = fopen("banco_objetos.txt", "r");//abre o arquivo com as palavras
+
+    if ((arq_ler3) == NULL) {
+      printf("Erro para abrir arquivo");
+      exit(1);
+    } else {
+      escolhePalavra("banco_animais.txt", escolha3);// chama a funcção para escolha aleatoria de palavras
+    }
+    strcpy(p3, escolhePalavra("banco_animais.txt", escolha3));//Copia o valor para a palavra a ser utilizada
+
+    printf("A palavra tem %lu caracteres.\n", strlen(p3));
+
+    char tela3[100];
+    strcpy(tela3, p3);
+
+    for (int i = 0; i < strlen(tela3); i++) {
+      tela3[i] = '_';
+    }
+
+    int erros3 = 0;
+    while (1) {
+
+      // imprime a forca
+      forca(erros3);
+
+      // imprime os underlines para cada letra
+      printf("\nAdvinhe:  ");
+      for (int i = 0; i < strlen(tela3); i++) {
+        printf("%c ", tela3[i]);
+      }
+
+      // recebe a letra
+      printf("\n\nLetra: ");
+      scanf(" %c", &letra3);
+
+      // verifica se a letra esta na palavra
+
+      int possivel_erro3 = 1; // 1 = sim, 0 = não
+      for (int i = 0; i < strlen(tela3); i++) {
+
+        if (letra3 == p3[i]) { // a letra ta certa.
+          tela3[i] = letra3;
+          possivel_erro3 = 0;
+        }
+      }
+
+      if (possivel_erro3 == 1) {
+        erros3++;
+      }
+
+      // verifica se o jogo acabou por erro
+      if (erros3 == 6) { // perdeu
+        forca(erros3);
+        break;
+      }
+
+      // verifica se tela == palavra, logo, jogo ganho.
+      if (strcmp(tela3, p3) == 0) {
+        printf("\nPalavra:  ");
+        for (int i = 0; i < strlen(tela3); i++) {
+          printf("%c ", tela3[i]);
+        }
+      printf("\n\n    *  *  *\n!!! VITÓRIA !!!\n    *  *  *\n");
+        break;
+      }}
+       fclose(arq_ler3);
+    break;
+    
+    case 4:
+
+      
+    printf("\n\n--------------------------------\n Seja Bem-Vindo as Configurações\n-------------------------------- ");
+  
+  do{
+    printf("\n\n ------ MENU ------ \n*opções de configuração*\n\n [ 1 ] - Adjetivos \n [ 2 ] - Objetos \n [ 3 ] - Animais"); 
+    printf("\n -    -    -    - \n Digite uma das opções para adicionar palavras: ");
+    scanf("%d", &opcao2);
+
+    switch(opcao2){
+
+      case 1:
+
+      arq = fopen("banco_adjetivos.txt","a");
+        
+      do {
+      printf("\n--------------------------------\n\n[ Digite 'sair' para "
+             "encerrar ]");
+
+      printf("\nQual palavra deseja adicionar? ");
+      scanf("%s", palavra); //recebe a palavra a ser gravada
+
+      strc = strcmp(palavra, "sair"); //Compara a string e retorna um valor para o encerramento ou não.
+
+      if (strc == 0) {
+        //encerra a configuração
+        printf("\n--------------------------------------\nObrigado por "
+               "adicionar novas "
+               "palavras!\n--------------------------------------"
+               "\n\nConfiguração encerrada.");
+      } else {
+
+        fputs(palavra, arq); //escreve a palavra no arquivo
+        fputs("\n", arq); // adiciona quebra de linha
+      }
+
+    } while (strc != 0);
+
+    fclose(arq); // fecha o arquivo de escrita
+
+    break;
+        
+    case 2:
+      
+       arq = fopen("banco_objetos.txt","a");
+        
+      do {
+      printf("\n--------------------------------\n\n[ Digite 'sair' para "
+             "encerrar ]");
+
+      printf("\nQual palavra deseja adicionar? ");
+      scanf("%s", palavra); //recebe a palavra a ser gravada
+
+      strc = strcmp(palavra, "sair"); //Compara a string e retorna um valor para o encerramento ou não.
+
+      if (strc == 0) {
+        //encerra a configuração
+        printf("\n--------------------------------------\nObrigado poradicionar novas palavras!\n--------------------------------------\n\nConfiguração encerrada.");
+      } else {
+
+        fputs(palavra, arq); //escreve a palavra no arquivo
+        fputs("\n", arq); // adiciona quebra de linha
+      }
+
+    } while (strc != 0);
+
+    fclose(arq); // fecha o arquivo de escrita
 
     break;
 
-  default:
-    printf("Valor não encontrado!");
+    case 3:
+       arq = fopen("banco_animais.txt","a");
+        
+      do {
+      printf("\n--------------------------------\n\n[ Digite 'sair' para "
+             "encerrar ]");
+
+      printf("\nQual palavra deseja adicionar? ");
+      scanf("%s", palavra); //recebe a palavra a ser gravada
+
+      strc = strcmp(palavra, "sair"); //Compara a string e retorna um valor para o encerramento ou não.
+
+      if (strc == 0) {
+        //encerra a configuração
+        printf("\n--------------------------------------\nObrigado por "
+               "adicionar novas "
+               "palavras!\n--------------------------------------"
+               "\n\nConfiguração encerrada.");
+      } else {
+
+        fputs(palavra, arq); //escreve a palavra no arquivo
+        fputs("\n", arq); // adiciona quebra de linha
+      }
+
+    } while (strc != 0);
+
+    fclose(arq); // fecha o arquivo de escrita
+
+    break;
+
+      default:
+
+      printf("* Valor não encontrado *");
+      printf("Deseja digitar novamente (s ou n)?");
+      scanf(" %c", &resp);
+
+    }
+  }while(resp=='s');
+      
+   
+  }//switch case 1
+
+  int strc1;
+    
+  printf("\n-----------------------------------\nDeseja Jogar novamente (s ou n) ?");
+  scanf(" %c", &repeat);
+
+  printf("\n-----------------------------------");
+  if (repeat =='n'){
+    printf("\n-----------------------------------\nMUITO OBRIGADO POR TER JOGADO!\n-----------------------------------");
+    break;
   }
+
+  }while(repeat =='s');   
+
+  
 }
